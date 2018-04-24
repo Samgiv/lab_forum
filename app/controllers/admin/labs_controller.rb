4af34423908e2a29565d0c824ad1033618747cfe
Samgiv <ken781222@gmail.com>
2018-04-24 15:37:19 +0800

    set up admin authentication

--- app/controllers/admin/labs_controller.rb
+++ app/controllers/admin/labs_controller.rb
@@ -1,5 +1,19 @@
 class Admin::LabsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+
   def index
     
   end
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "You aren't Admin!"
+      redirect_to root_path
+    end
+    
+  end
+
 end
